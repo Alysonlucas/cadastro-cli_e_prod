bcbc32de7803dd3552288f99c6daeec26c4a69a3
Alysonlucas <155203717+Alysonlucas@users.noreply.github.com>
2025-10-15 09:08:38 -0300

    Add files via upload

---
+++ js/javinha.js
@@ -0,0 +1,9 @@
+function lista(){
+    alert("Dados encontrados");
+    window.location.href = "listar_cli.php";
+}
+
+function listarProdutos() {
+    alert("Dados encontrados");
+    window.location.href="listarproduto.php"
+}
